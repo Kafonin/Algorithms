83ab636b636a421190b38cd84a318d164f91ba59
Kenny J Afonin <40528652+Kafonin@users.noreply.github.com>
2021-02-18 20:52:57 -0500

    Time: 0 ms (100.00%), Space: 37.6 MB (45.23%) - LeetHub

--- sort-colors/sort-colors.java
+++ sort-colors/sort-colors.java
@@ -1,18 +1,19 @@
 class Solution {
     public void sortColors(int[] nums) {
-        sortThis(nums);
-    }
-    
-    public int[] sortThis(int[] arr) {
-        for (int i=0; i<arr.length; i++) {
-            for (int j=i+1; j<arr.length; j++) {
-                if (arr[i]>arr[j]) {
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
+        int index = 0, start = 0, end = nums.length-1;
+        while (index<=end && start<end) {
+            if (nums[index]==0) {
+                nums[index] = nums[start];
+                nums[start] = 0;
+                index++;
+                start++;
+            } else if (nums[index]==2) {
+                nums[index] = nums[end];
+                nums[end] = 2;
+                end--;
+            } else {
+                index++;
             }
         }
-        return arr;
     }
 }
