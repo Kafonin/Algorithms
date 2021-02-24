01d9685685f761e21bd5d9c1019eb224456b5484
Kenny J Afonin <40528652+Kafonin@users.noreply.github.com>
2021-02-24 10:34:17 -0500

    Update find-the-duplicate-number.java

--- find-the-duplicate-number/find-the-duplicate-number.java
+++ find-the-duplicate-number/find-the-duplicate-number.java
@@ -1,13 +1,30 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int n : nums) {
-            if (!map.containsKey(n)) {
-                map.put(n, map.getOrDefault(n, 0)+1);
-            } else {
-                return n;
-            }
+        int slow = nums[0];
+        int fast = nums[0];
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow!=fast);
+        slow = nums[0];
+        while (slow!=fast) {
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return 0;
+        return fast;
     }
 }
+
+// class Solution {
+//     public int findDuplicate(int[] nums) {
+//         Map<Integer, Integer> map = new HashMap<>();
+//         for (int n : nums) {
+//             if (!map.containsKey(n)) {
+//                 map.put(n, map.getOrDefault(n, 0)+1);
+//             } else {
+//                 return n;
+//             }
+//         }
+//         return 0;
+//     }
+// }
