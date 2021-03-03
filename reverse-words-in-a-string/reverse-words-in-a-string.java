482d89e86cb24f25771d5817aec6f73fb63097e8
Kenny J Afonin <40528652+Kafonin@users.noreply.github.com>
2021-03-02 23:27:27 -0500

    Time: 5 ms (70.23%), Space: 39 MB (84.25%) - LeetHub

--- reverse-words-in-a-string/reverse-words-in-a-string.java
+++ reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,18 +1,49 @@
 class Solution {
     public String reverseWords(String s) {
-        Stack<String> stack = new Stack<String>();
-        String[] temp = s.trim().split(" ");
-        for (String str : temp) {
-            if (!str.isEmpty())
-                stack.push(str);
-        }
-        
+        ArrayList<String> al = stringList(s);
+        // String[] temp = s.split("\\s+");
         StringBuilder sb = new StringBuilder();
-        while (!stack.isEmpty()) {
-            sb.append(stack.pop());
-            sb.append(" ");            
+        for (int i=al.size()-1; i>=0; i--) {
+            sb.append(al.get(i));
+            if (i>0) sb.append(" ");
         }
-        
-        return sb.toString().trim();
+        return sb.toString();
+    }
+    
+    public ArrayList<String> stringList(String stringParameter) {
+        int len = 0;
+        StringBuilder sb = new StringBuilder();
+        ArrayList<String> res = new ArrayList();
+        while (len<stringParameter.length()) {
+            while (len<stringParameter.length() && Character.isLetterOrDigit(stringParameter.charAt(len))) {
+                sb.append(stringParameter.charAt(len));
+                len++;
+            }
+            if (sb.length()>0) {
+                res.add(sb.toString());
+                sb.setLength(0);
+            }
+            len++;
+        }
+        return res;
     }
 }
+
+// class Solution {
+//     public String reverseWords(String s) {
+//         Stack<String> stack = new Stack<String>();
+//         String[] temp = s.trim().split(" ");
+//         for (String str : temp) {
+//             if (!str.isEmpty())
+//                 stack.push(str);
+//         }
+        
+//         StringBuilder sb = new StringBuilder();
+//         while (!stack.isEmpty()) {
+//             sb.append(stack.pop());
+//             sb.append(" ");            
+//         }
+        
+//         return sb.toString().trim();
+//     }
+// }
