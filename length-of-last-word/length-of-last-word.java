3a72283758fde0e513faaa82fe3457c936bd98a3
Kenny J Afonin <40528652+Kafonin@users.noreply.github.com>
2021-03-25 16:44:30 -0400

    Update length-of-last-word.java

--- length-of-last-word/length-of-last-word.java
+++ length-of-last-word/length-of-last-word.java
@@ -1,7 +1,23 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] words = s.split(" ");
-        if (words.length==0) return 0;
-        return words[words.length-1].length();
+        int len = s.length();
+        int count = 0;
+        
+        while (len>0 && s.charAt(len-1)==' ') {
+            len--;
+        }
+        while (len>0 && s.charAt(len-1)!=' ') {
+            count++;
+            len--;
+        }
+        return count;
     }
 }
+
+// class Solution {
+//     public int lengthOfLastWord(String s) {
+//         String[] words = s.split(" ");
+//         if (words.length==0) return 0;
+//         return words[words.length-1].length();
+//     }
+// }
