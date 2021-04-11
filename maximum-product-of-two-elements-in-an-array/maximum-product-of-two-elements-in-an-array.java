66f6104c93491a318e79feedd2c1c044f2b400ca
Kenny J Afonin <40528652+Kafonin@users.noreply.github.com>
2021-04-10 22:21:15 -0400

    Update maximum-product-of-two-elements-in-an-array.java

--- maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.java
+++ maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.java
@@ -1,6 +1,22 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        Arrays.sort(nums);
-        return (nums[nums.length-1] - 1) * (nums[nums.length-2] - 1);
+        int i = Integer.MIN_VALUE, j = i;
+        
+        for (int num : nums) {
+            if (num >= i) {
+                j = i;
+                i = num;
+            } else if (num > j) {
+                j = num;
+            }
+        }
+        return (i-1)*(j-1);
     }
 }
+
+// class Solution {
+//     public int maxProduct(int[] nums) {
+//         Arrays.sort(nums);
+//         return (nums[nums.length-1] - 1) * (nums[nums.length-2] - 1);
+//     }
+// }
