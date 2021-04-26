c608db4449cceefe7af1dcfac9c0cb9543b6506f
Kenny J Afonin <40528652+Kafonin@users.noreply.github.com>
2021-04-26 11:39:55 -0400

    Update ransom-note.java

--- ransom-note/ransom-note.java
+++ ransom-note/ransom-note.java
@@ -1,16 +1,37 @@
 class Solution {
-    public boolean canConstruct(String ransomNote, String magazine) {
-        int[] arr = new int[26];
+    public boolean canConstruct(String ransomNote, String magazine) {        
+        int[] arr1 = new int[26];
+
         
-        for (int i=0; i<magazine.length(); i++) {
-            arr[magazine.charAt(i) - 'a']++;
+        for (char c : magazine.toCharArray()) {
+            ++arr1[c - 'a'];
         }
-        for (int i=0; i<ransomNote.length(); i++) {
-            arr[ransomNote.charAt(i) - 'a']--;
+        
+        for (char c : ransomNote.toCharArray()) {
+            if (--arr1[c - 'a'] < 0) {
+                return false;
+            }
         }
-        for (int j=0; j<arr.length; j++) {
-            if (arr[j]<0) return false;
-        }
+        
+        
         return true;
     }
 }
+
+
+// class Solution {
+//     public boolean canConstruct(String ransomNote, String magazine) {
+//         int[] arr = new int[26];
+        
+//         for (int i=0; i<magazine.length(); i++) {
+//             arr[magazine.charAt(i) - 'a']++;
+//         }
+//         for (int i=0; i<ransomNote.length(); i++) {
+//             arr[ransomNote.charAt(i) - 'a']--;
+//         }
+//         for (int j=0; j<arr.length; j++) {
+//             if (arr[j]<0) return false;
+//         }
+//         return true;
+//     }
+// }
