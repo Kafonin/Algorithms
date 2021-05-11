1323d5e1162038d43d58be5fd5adbe76f3bce792
Kenny J Afonin <40528652+Kafonin@users.noreply.github.com>
2021-05-11 01:14:33 -0400

    Time: 0 ms (100.00%), Space: 39.4 MB (41.18%) - LeetHub

--- missing-number/missing-number.java
+++ missing-number/missing-number.java
@@ -1,13 +1,10 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int sum1 = 0, sum2 = 0, j=1;
+        int sum1 = nums[0], sum2 = nums[0], j = 1;
         for (int i=0; i<nums.length; i++) {
-            sum1 += nums[i];
-            sum2 += j;
-            j++;
+            sum1+=nums[i];
+            sum2+=j++;
         }
-        System.out.println("sum1: " + (sum1));
-        System.out.println("sum2: " + sum2);
         return sum2-sum1;
     }
 }
