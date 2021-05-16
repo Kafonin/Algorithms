8cf2fddefea07ac57ba6f549a4b0978226175035
Kenny J Afonin <40528652+Kafonin@users.noreply.github.com>
2021-05-15 23:34:34 -0400

    Time: 0 ms (100.00%), Space: 38.7 MB (38.01%) - LeetHub

--- best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,11 +1,11 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int sum = 0;
-        for (int i=0, j=1; j<prices.length; i++, j++) {
-            if (prices[j]>prices[i]) {
-                sum+=prices[j]-prices[i];
+        int total = 0;
+        for (int i=0; i<prices.length-1; i++) {
+            if (prices[i]<prices[i+1]) {
+                total += prices[i+1]-prices[i];
             }
         }
-        return sum;
+        return total;
     }
 }
