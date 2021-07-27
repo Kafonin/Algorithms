5e464a0423ebb4677ac113eff6887bb1dd09235a
Kenny J Afonin <40528652+Kafonin@users.noreply.github.com>
2021-07-27 00:22:31 -0400

    Time: 0 ms (100.00%), Space: 40 MB (71.14%) - LeetHub

--- linked-list-cycle/linked-list-cycle.java
+++ linked-list-cycle/linked-list-cycle.java
@@ -15,9 +15,10 @@ public class Solution {
         ListNode fast = head;
         
         while (fast!=null && fast.next!=null) {
-            fast = fast.next.next;
+            
             slow = slow.next;
-            if (fast == slow) return true;
+            fast = fast.next.next;
+            if (slow==fast) return true;
         }
         return false;
     }
