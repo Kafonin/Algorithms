ec37bc04961b9d443fa843c0ff2738e4318b69af
Kenny J Afonin <40528652+Kafonin@users.noreply.github.com>
2022-01-29 00:50:52 -0500

    Time: 1 ms, Memory: 43.7 MB - LeetHub

--- move-zeroes/move-zeroes.java
+++ move-zeroes/move-zeroes.java
@@ -1,11 +1,12 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int i=0, j=0;
-        while (i<nums.length && j<nums.length) {
+        
+        while (j<nums.length) {
             if (nums[j]!=0) {
                 int temp = nums[i];
-                nums[i]  = nums[j];
-                nums[j]  = temp;
+                nums[i] = nums[j];
+                nums[j] = temp;
                 i++;
                 j++;
             } else if (i<nums.length && nums[i]!=0) {
@@ -13,6 +14,7 @@ class Solution {
             } else if (j<nums.length && nums[j]==0) {
                 j++;
             }
+            
         }
     }
 }
