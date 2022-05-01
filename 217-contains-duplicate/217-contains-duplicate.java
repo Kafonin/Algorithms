2270fa959c6fc75a7154c30069ea882b7755d27a
Kenny J Afonin <40528652+Kafonin@users.noreply.github.com>
2022-04-30 22:19:02 -0400

    Time: 6 ms (94.76%), Space: 72 MB (12.60%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.java
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -1,10 +1,9 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        
-        for (int i=0; i<nums.length-1; i++) {
-            if (nums[i]==nums[i+1]) return true;
-        }
-        return false;
-    }
+	HashSet<Integer> set = new HashSet<Integer>();
+	for(int i=0;i<nums.length;i++){
+		if(!set.add(nums[i])) return true;
+	}
+	return false;
+}
 }
