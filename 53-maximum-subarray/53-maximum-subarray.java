3d1dd465b3a12de5c36afd66d3cabbae502f3a29
Kenny J Afonin <40528652+Kafonin@users.noreply.github.com>
2022-05-02 01:31:28 -0400

    Time: 2 ms (56.11%), Space: 74.3 MB (32.62%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -5,11 +5,11 @@ class Solution {
         int sum = 0;
         
         for (int i = 0; i < nums.length; i++) {
-            // placing this if statement on top makes code faster by 1 ms
+
             if (sum < 0) {
                 sum = 0;
             }
-            // code below is faster by 1 ms than sum = sum + nums[i];
+
             sum += nums[i];
             mx  = Math.max(mx, sum);       
         }
