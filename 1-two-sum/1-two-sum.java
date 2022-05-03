48861b5c3dc3a22fc30e756255612da851e93e47
Kenny J Afonin <40528652+Kafonin@users.noreply.github.com>
2022-05-03 01:08:09 -0400

    Time: 0 ms (100.00%), Space: 45.2 MB (49.74%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,12 +1,26 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for (int i = 1; i < nums.length; i++) {
-            for (int j = i; j < nums.length; j++) {
-                if (nums[j] + nums[j-i] == target) {
-                    return new int[]{j, j-i};
+
+        for (int index = 1; index < nums.length; index++) {
+            for (int j = index; j < nums.length; j++) {
+                if (nums[j] + nums[j-index] == target) {
+                    return new int[]{j, j-index};
                 }
             }
         }
         throw new IllegalArgumentException();
     }
 }
+
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         for (int i = 1; i < nums.length; i++) {
+//             for (int j = i; j < nums.length; j++) {
+//                 if (nums[j] + nums[j-i] == target) {
+//                     return new int[]{j, j-i};
+//                 }
+//             }
+//         }
+//         throw new IllegalArgumentException();
+//     }
+// }
