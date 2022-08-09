df3997f3c78ecaf7551c336cd2c4f944e3823390
Kenny J Afonin <40528652+Kafonin@users.noreply.github.com>
2022-08-08 23:00:53 -0400

    Time: 19 ms (20.74%), Space: 42.6 MB (7.02%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,13 +1,16 @@
 class Solution {
     public int rob(int[] nums) {
         int rob1 = 0, rob2 = 0;
-        
         for (int n : nums) {
             int temp = Math.max(n + rob1, rob2);
+            System.out.println("temp: " + temp);
             rob1 = rob2;
+            System.out.println("rob1: " + rob1);
             rob2 = temp;
+            System.out.println("rob2: " + rob2);
         }
         
         return rob2;
     }
 }
+//1,
