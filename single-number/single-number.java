46b7308aecaf5f6d6c27f8f4000698f231b62867
Kenny J Afonin <40528652+Kafonin@users.noreply.github.com>
2023-08-07 01:12:46 -0400

    Time: 1 ms, Memory: 45 MB - LeetHub

--- single-number/single-number.java
+++ single-number/single-number.java
@@ -1,26 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int n : nums) {
-            if (!map.containsKey(n)) {
-                map.put(n, 1);
-            } else {
-                map.put(n, map.get(n)+1);
-            }
+        int k = 0;
+        for (int i : nums) {
+            k ^= i;
         }
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue()==1) return entry.getKey();
-        }
-        return -1;
+        return k;
     }
 }
-
-// class Solution {
-//     public int singleNumber(int[] nums) {
-//         int n = 0;
-//         for (int num : nums) {
-//             n^=num;
-//         }
-//         return n;
-//     }
-// }
